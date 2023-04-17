dc479f53f42354139a28973ca6295c0e0d6c5383
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-04-17 11:14:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ function straight () {
 }
 basic.showIcon(IconNames.Yes)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 0) {
         straight()
